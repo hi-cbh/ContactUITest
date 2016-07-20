80269070205c56da65262aea3148bdd0dda57be5
hi-cbh <hi_cbh@qq.com>
2016-07-20 16:38:45 +0800

    微量更新

--- src/com/uitest/data/UserConfig.java
+++ src/com/uitest/data/UserConfig.java
@@ -9,6 +9,7 @@ public class UserConfig {
 	public static String LoginName = "13427665104";
 	public static String LoginPwd = "yscs12345";
 
+	//根据每台PC的不同，进行调整
 	public static String androidId = "2";
 	public static String packageName = "com.chinamobile.contacts.im";
 	
