024c1c67ccaf8b6f8079a9b20fa2e2f9410a4736
hi-cbh <hi_cbh@qq.com>
2016-10-14 17:05:01 +0800

    cmd line change

--- src/com/uitest/commandline/Command.java
+++ src/com/uitest/commandline/Command.java
@@ -2,6 +2,8 @@ package com.uitest.commandline;
 
 import java.io.IOException;
 
+import com.uitest.data.UserConfig;
+
 public class Command
 {
 	/**
@@ -18,6 +20,42 @@ public class Command
 
     public static void callPhone(String phone){
     	cmdLine("am start -a android.intent.action.CALL -d tel:"+phone);
+    }
+    
+    /**
+     * 
+     * @param num
+     */
+    public static void setShurufa(int num){
+    	switch (num) {
+		case 1:
+			cmdLine("adb shell ime set jp.jun_nama.test.utf7ime/.Utf7ImeService");
+			break;
+
+		case 2:
+			cmdLine("com.samsung.inputmethod/.SamsungIME");
+			break;
+		default:
+			break;
+		}
+
+    	//使用adb shell ime list -s 列出输入法列表
+    	
+    }
+    
+    /**
+     * 清除和通讯录缓存
+     */
+    public static void clear(){
+    	cmdLine("adb shell pm clear " + UserConfig.packageName);
+    }
+    
+    
+    /**
+     * 停止脚本运行
+     */
+    public static void stop(){
+    	cmdLine("adb shell am force-stop " + UserConfig.packageName);
     }
     
     
